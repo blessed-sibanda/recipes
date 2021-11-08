1acee5e7028b909e6a4dbd1667187dfff2c0f11c
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-08 18:01:43 +0200

    create ui models and data repository

---
+++ lib/data/models/recipe.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+import 'package:recipes/data/models/ingredient.dart';
+
+class Recipe extends Equatable {
+  int id;
+  final String label;
+  final String image;
+  final String url;
+
+  List<Ingredient> ingredients = [];
+  final double calories;
+  final double totalWeight;
+  final double totalTime;
+
+  Recipe({
+    required this.id,
+    required this.label,
+    required this.image,
+    required this.url,
+    required this.calories,
+    required this.totalWeight,
+    required this.totalTime,
+  });
+
+  @override
+  List<Object> get props =>
+      [label, image, url, calories, totalWeight, totalTime];
+}
