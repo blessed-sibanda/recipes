7268978a589b07026cb16ed01f2ef60e2ef9f551
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-08 16:50:47 +0200

    setup chopper client and setup logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:logging/logging.dart';
 
 import 'ui/main_screen.dart';
 
 Future<void> main() async {
+  _setupLogging();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
+}
+
+void _setupLogging() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((event) {
+    print('${event.level.name}: ${event.time}: ${event.message}');
+  });
 }
 
 class MyApp extends StatelessWidget {
--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -1,24 +1,36 @@
-import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart';
+import 'package:chopper/chopper.dart';
+import 'package:recipes/network/model_response.dart';
+import '../network/recipe_model.dart';
+import 'model_converter.dart';
+
+part 'recipe_service.chopper.dart';
 
 const String apiKey = '099da388b47c054c56b12d2a02fff1ee';
 const String apiId = '75678367';
-var apiUrl = 'https://api.edamam.com/search';
+var apiUrl = 'https://api.edamam.com';
 
-Future getData(String url) async {
-  print('Calling url: $url');
-  final response = await get(Uri.parse(url));
-  if (response.statusCode == 200) {
-    return response.body;
-  } else {
-    print(response.statusCode);
+@ChopperApi()
+abstract class RecipeService extends ChopperService {
+  @Get(path: 'search')
+  Future<Response<Result<APIRecipeQuery>>> queryRecipes(
+      @Query('q') String query, @Query('from') int from, @Query('to') int to);
+
+  static RecipeService create() {
+    final client = ChopperClient(
+        baseUrl: apiUrl,
+        interceptors: [_addQuery, HttpLoggingInterceptor],
+        converter: ModelConverter(),
+        errorConverter: const JsonConverter(),
+        services: [
+          _$RecipeService(),
+        ]);
+    return _$RecipeService(client);
   }
 }
 
-class RecipeService {
-  Future<dynamic> getRecipes(String query, int from, int to) async {
-    final recipeData = await getData(
-        '$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to');
-    return recipeData;
-  }
+Request _addQuery(Request req) {
+  final params = Map<String, dynamic>.from(req.parameters);
+  params['app_id'] = apiId;
+  params['app_key'] = apiKey;
+  return req.copyWith(parameters: params);
 }
